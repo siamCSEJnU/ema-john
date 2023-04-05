290b64f4694bf8a5f39a14779641d4cefb7f6792
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-04-05 14:08:23 +0600

    route defined

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="header">
       <img src={logo} alt="" />
       <div>
-        <a href="/shop">shop</a>
-        <a href="/order">order</a>
-        <a href="/inventory">inventory</a>
-        <a href="/login">Login</a>
+        <Link to="/">shop</Link>
+        <Link to="/order">order</Link>
+        <Link to="/inventory">inventory</Link>
+        <Link to="/login">Login</Link>
       </div>
     </nav>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Shop from "./Components/Shop/Shop";
+import Order from "./Components/Order/Order";
+import Inventory from "./Components/Inventory/Inventory";
+import Login from "./Components/Login/Login";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "order",
+        element: <Order></Order>,
+      },
+      {
+        path: "inventory",
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
